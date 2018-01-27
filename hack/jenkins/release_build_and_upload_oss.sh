6bb4fe8bad58cffa48b26aa3160295053eb57448
Li Yi <denverdino@gmail.com>
2018-01-27 16:44:12 +0800

    Support K8S 1.9 release

--- hack/jenkins/release_build_and_upload_oss.sh
+++ hack/jenkins/release_build_and_upload_oss.sh
@@ -30,7 +30,7 @@ export BUCKET=kubernetes/minikube
 export TAGNAME=v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_BUILD}
 export DEB_VERSION=${VERSION_MAJOR}.${VERSION_MINOR}-${VERSION_BUILD}
 export GOPATH=~/go
-export K8SRELEASE=v1.8.0
+export K8SRELEASE=v1.9.0
 
 # Build all binaries in docker
 export BUILD_IN_DOCKER=y
