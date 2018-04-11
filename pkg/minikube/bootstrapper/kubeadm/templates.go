c454bc01f3de4c3867f30bafa30d8e6b41cb13d2
Li Yi <denverdino@gmail.com>
2018-04-11 20:06:18 +0800

    Support Aliyun mirror

--- pkg/minikube/bootstrapper/kubeadm/templates.go
+++ pkg/minikube/bootstrapper/kubeadm/templates.go
@@ -30,6 +30,7 @@ api:
   advertiseAddress: {{.AdvertiseAddress}}
   bindPort: {{.APIServerPort}}
 kubernetesVersion: {{.KubernetesVersion}}
+imageRepository: registry.cn-hangzhou.aliyuncs.com/google_containers
 certificatesDir: {{.CertDir}}
 networking:
   serviceSubnet: {{.ServiceCIDR}}
@@ -72,6 +73,8 @@ sudo /usr/bin/kubeadm alpha phase etcd local --config {{.KubeadmConfigFile}}
 `))
 
 var kubeadmInitTemplate = template.Must(template.New("kubeadmInitTemplate").Parse(
+	"sudo docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:3.1 && " +
+	"sudo docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:3.1 k8s.gcr.io/pause-amd64:3.1 && " + 
 	"sudo /usr/bin/kubeadm init --config {{.KubeadmConfigFile}} {{if .SkipPreflightChecks}}--skip-preflight-checks{{else}}{{range .Preflights}}--ignore-preflight-errors={{.}} {{end}}{{end}}"))
 
 // printMapInOrder sorts the keys and prints the map in order, combining key
